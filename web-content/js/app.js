19d8e85e0febaaafacafa0fcd95313d12af17879
Christopher Gosala <cgosala@deloitte.com>
2017-03-12 17:35:05 +0530

    Added css, module for presentation.

--- web-content/js/app.js
+++ web-content/js/app.js
@@ -1,4 +1,17 @@
-angular.module('my-presentation', [])
+angular.module('presentation-app', ['presentation'])
     .controller('mainCtrl', ['$scope', function(scope) {
         scope.test = 'Initial mark up with angular'
+    }]);
+
+angular.module('presentation', [])
+    .directive('container', [function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            templateUrl:'../partials/container.html',
+            compile: function(element, attr) {
+                element.addClass('maxed-out');
+                return function() {}
+            }
+        }
     }]);
