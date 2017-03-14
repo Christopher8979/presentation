14c63775e289831ebf2b4fd0acac3eef389af987
Christopher Gosala <cgosala@deloitte.com>
2017-03-15 01:01:54 +0530

    Complete flow of the presentation.

--- web-content/js/app.js
+++ web-content/js/app.js
@@ -10,16 +10,80 @@ angular.module('presentation', [])
     .directive('container', [function() {
         return {
             restrict: 'EA',
+            scope: {
+                slides: '='
+            },
             replace: true,
-            transclude: true,
             templateUrl: '../partials/container.html',
-            compile: function(element, attributes) {
-                return function(scope, elem, attr) {
-                    scope.maxed = true;
-                    scope.toggleScreen = function() {
-                        scope.maxed = !scope.maxed;
-                    };
+            controller: function($scope) {
+                var presentation = this;
+                presentation.slides = angular.copy($scope.slides);
+                presentation.maxed = true;
+                presentation.toggleScreen = function() {
+                    presentation.maxed = !presentation.maxed;
+                };
+                presentation.slidesRedefined = [];
+
+                presentation.chapterNo = 0;
+                presentation.slideNo = 0;
+                presentation.buttonStatus = {
+                    leftDisabled: false,
+                    upDisabled: false,
+                    downDisabled: false,
+                    rightDisabled: false
+                };
+
+                presentation.buttonStatusDefault = angular.copy(presentation.buttonStatus);
+
+
+                // Method to change slides into desired format
+                presentation.transformInput = function() {
+                    angular.forEach(presentation.slides, function(chapter, index) {
+                        presentation.slidesRedefined.push({
+                            'chapter': chapter,
+                            'length': chapter.length
+                        });
+                    });
+                    presentation.slides = angular.copy(presentation.slidesRedefined);
                 }
+
+
+                presentation.checkButtonStatus = function() {
+                    presentation.buttonStatus = angular.copy(presentation.buttonStatusDefault);
+
+                    if (0 === presentation.chapterNo) {
+                        presentation.buttonStatus.leftDisabled = true;
+                    }
+                    if (0 === presentation.slideNo) {
+                        presentation.buttonStatus.upDisabled = true;
+                    }
+                    if ((presentation.slides[presentation.chapterNo].length - 1) === presentation.slideNo) {
+                        presentation.buttonStatus.downDisabled = true;
+                    }
+                    if ((presentation.slides.length - 1) === presentation.chapterNo) {
+                        presentation.buttonStatus.rightDisabled = true;
+                    }
+                };
+
+                presentation.checkButtonStatus();
+
+                presentation.prevChapter = function() {
+                    presentation.chapterNo--;
+                };
+                presentation.prevSlide = function() {
+                    presentation.slideNo--;
+                };
+                presentation.nextSlide = function() {
+                    presentation.slideNo++;
+                };
+                presentation.nextChapter = function() {
+                    presentation.chapterNo++;
+                };
+
+            },
+            controllerAs: 'container',
+            compile: function(element, attributes) {
+                return function(scope, elem, attr) {}
             }
         }
     }])
@@ -33,12 +97,12 @@ angular.module('presentation', [])
             templateUrl: '../partials/slides-holder.html',
             link: function(scope, elem, attr) {
 
-                scope.chapterNo = 0;
-                scope.slideNo = 0;
-
-                scope.nextChapter = function() {
-                    scope.chapterNo++;
-                };
+                // scope.chapterNo = 0;
+                // scope.slideNo = 0;
+                //
+                // scope.nextChapter = function() {
+                //     scope.chapterNo++;
+                // };
 
                 console.log(scope);
                 console.log(attr);
